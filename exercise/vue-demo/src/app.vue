3c578549b674e85e448c190084442cef4f5656c6
jeannewu <454106678@qq.com>
2020-03-17 11:56:57 +0800

    新增解析es6语法的babel

---
+++ exercise/vue-demo/src/app.vue
@@ -0,0 +1,27 @@
+
+
+<!--
+ * @Description: This is a test vue file
+ * @Author: JeanneWu
+ * @Date: 2020-03-17 11:52:58
+ -->
+// app.vue
+<template>
+    <div class="test">{{text}}</div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                text:'hello webpack111!！！'
+            }
+        }
+    }
+</script>
+<style>
+    .text{
+        color: #999;
+    }
+</style>
+
+
--- exercise/vue-demo/src/main.js
+++ exercise/vue-demo/src/main.js
@@ -1,9 +1,19 @@
 
-const vue = require('vue')
+
+/*
+ * @Description: This is a XX file
+ * @Author: JeanneWu
+ * @Date: 2020-03-17 09:13:53
+ */
+// js文件头部注释之后的内容
+import Vue from 'vue'
+import App from './app.vue'
+
+
+let root = document.createElement('div');
+document.body.appendChild(root);
+
 
 new Vue({
-    el: '#app',
-    data: {
-        message: "welcome vue.js"
-    }
-})
+	render: (h)=> h(App)
+}).$mount(root)
